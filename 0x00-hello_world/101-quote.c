0694a13475f599ef97b1a1e6bf1456d62335d010
bvddxxur <anung.un.rama.hellboy@hotmail.fr>
2019-10-20 06:50:53 +0100

    new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char text1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char text1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fprintf(stderr, "%s", text1);
 return (1);
 }
