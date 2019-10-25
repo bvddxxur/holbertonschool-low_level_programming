9a929a7d19fa8bde9c13eccc4a2d4fdb7ed5b9be
bvddxxur <anung.un.rama.hellboy@hotmail.fr>
2019-10-25 15:44:22 +0100

    old debt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-char text1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fprintf(stderr, "%s", text1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
